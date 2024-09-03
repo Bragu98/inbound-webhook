cf400a95a917c74a15fc9897f52301ccc756802f
Brayan Guecha <brayanguecha01@gmail.com>
2024-09-02 21:20:00 -0500

    fix service

--- src/module/inbound-webhook/inbound-webhook.service.ts
+++ src/module/inbound-webhook/inbound-webhook.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateInboundWebhookDto } from './dto/create-inbound-webhook.dto';
-import { UpdateInboundWebhookDto } from './dto/update-inbound-webhook.dto';
+
 
 @Injectable()
 export class InboundWebhookService {
-  create(createInboundWebhookDto: CreateInboundWebhookDto) {
-    return 'This action adds a new inboundWebhook';
-  }
-
-  findAll() {
-    return `This action returns all inboundWebhook`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} inboundWebhook`;
-  }
-
-  update(id: number, updateInboundWebhookDto: UpdateInboundWebhookDto) {
-    return `This action updates a #${id} inboundWebhook`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} inboundWebhook`;
+  create(data: any) {
+    return data;
   }
 }
